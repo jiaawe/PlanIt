e9df9146788d02da5557a8e583e73d3375e207d2
Wai Sheen Chong <chong.wai.sheen@gmail.com>
2023-03-13 00:23:10 +0800

    created task details page

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,22 @@
   <main>
     <ManagerNavBar />
     <HomePage />
+    <!-- <ProfilePage /> -->
+    <!-- <CreateProject /> -->
+    <!-- <TaskDetailsPage /> -->
   </main>
 </template>
 
 <script>
 import ManagerNavBar from "@/components/ManagerNavBar.vue";
+import EmployeeNavBarVue from '@/components/EmployeeNavBar.vue';
 import ProfilePage from "@/views/ProfilePage.vue";
 import HomePage from "@/views/HomePage.vue";
-import CreateProject from "./views/CreateProject.vue";
-import EmployeeNavBarVue from './components/EmployeeNavBar.vue';
+import CreateProject from "@/views/CreateProject.vue";
+import TaskDetailsPage from "@/views/TaskDetailsPage.vue";
+
 
 export default {
-  components: { ProfilePage, HomePage, CreateProject, ManagerNavBar, EmployeeNavBarVue},
+  components: { ProfilePage, HomePage, CreateProject, ManagerNavBar, EmployeeNavBarVue, TaskDetailsPage},
 };
 </script>
